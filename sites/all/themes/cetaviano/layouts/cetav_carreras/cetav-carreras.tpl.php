364083aa96397603ce6b5e117122b759c95b7aaa
ialfaro <iaf643@gmail.com>
2015-12-04 15:43:25 -0600

    added class form panel carreras

--- sites/all/themes/cetaviano/layouts/cetav_carreras/cetav-carreras.tpl.php
+++ sites/all/themes/cetaviano/layouts/cetav_carreras/cetav-carreras.tpl.php
@@ -8,22 +8,26 @@
 ?>
 <div class="panel-display panel-cetav-carreras">
   <?php if ($content['wide']): ?>
-    <div class="panel-container panel-wide">
+    <div class="panel-container panel-wide panel-header">
       <?php print $content['wide']; ?>
     </div>
   <?php endif ?>
   <div class="center-wrapper">
-    <div class="panel-container panel-left-column">
-      <?php print $content['left']; ?>
-    </div>
-    <div class="panel-container panel-right-column">
-      <?php print $content['right']; ?>
+    <div class="grid-width">
+      <div class="panel-container panel-left-column">
+        <?php print $content['left']; ?>
+      </div>
+      <div class="panel-container panel-right-column">
+        <?php print $content['right']; ?>
+      </div>
     </div>
   </div>
   <?php if ($content['wide_second']): ?>
     <div class="panel-container panel-wide">
-      <?php print $content['wide_second']; ?>
+    <div class="grid-width">
+         <?php print $content['wide_second']; ?>
     </div>
+       </div>
   <?php endif ?>
   <div class="center-wrapper">
     <div class="panel-container panel-left-column">
@@ -34,8 +38,10 @@
     </div>
   </div>
  <?php if ($content['wide_third']): ?>
-    <div class="panel-container panel-wide">
+    <div class="panel-container panel-wide section-skills">
+    <div class="grid-width">
       <?php print $content['wide_third']; ?>
+    </div>
     </div>
   <?php endif ?>
 </div>
