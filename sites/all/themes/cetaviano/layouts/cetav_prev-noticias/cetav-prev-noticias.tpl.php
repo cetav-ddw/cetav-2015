c4fd9a77687c0c2fb6c1a4c14c55e1ad613574c2
ialfaro <iaf643@gmail.com>
2015-12-09 14:05:49 -0600

    fixed php error from custom panel

---
+++ sites/all/themes/cetaviano/layouts/cetav_prev-noticias/cetav-prev-noticias.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Template for a 3 column panel layout.
+ *
+ * This template provides a prev-egresados for CETAV panel display layout.
+ */
+?>
+<div class="panel-display panel-cetav-noticias">
+  <div class="center-wrapper">
+  <div class="grid-width">
+    <div class="panel-container panel-left-column container-news">
+      <?php print $content['left']; ?>
+    </div>
+    <div class="panel-container panel-right-column aside">
+      <?php print $content['right']; ?>
+    </div>
+  </div>
+  </div>
+</div>
