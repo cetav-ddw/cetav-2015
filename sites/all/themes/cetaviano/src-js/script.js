73ebefbd4e57b1ba6f60b4b6fedb96935d5a9014
stuartesh <stuart.sarmiento@gmail.com>
2015-12-09 12:39:44 -0600

    Estilos de cursos libres: Formulario, botones, posicionamiento y listas

--- sites/all/themes/cetaviano/src-js/script.js
+++ sites/all/themes/cetaviano/src-js/script.js
@@ -1,6 +1,26 @@
 // Manatí Base Theme JS functions
 (function($) {
+  $(function() { 
+    function breakList(numOfLists, list){
+      var listLength = list.find("li").length;
+      var numInRow = 11; // Num of desired items on each column.
+      var listItems; // list of the sliced items.
+      var newList; // New lists wrapper.
+      var i = 0;
+      for (i;i<numOfLists;i++){
+        listItems = list.find("li").slice(0, numInRow); // Gest the desired items for each column.
+        newList = $("<ul/>").append(listItems); // Wrap them into another "ul".
+        $(".split-list").append(newList); // Unwrap the items into the desired selector.
+      }
+      $(".split-list > ul:first-child").remove(); // Removes the empty list.
+    }
+    breakList(3, $(".split-list ul"));
 
-// Add code here
-
+    $("#show-pay-form").click(function () {
+        $(this).toggleClass("hide-button");
+        $("#course-form").toggleClass("show-course-form");
+        $("#show-course-form").remove();
+    });
+    
+  });
 })(jQuery);
