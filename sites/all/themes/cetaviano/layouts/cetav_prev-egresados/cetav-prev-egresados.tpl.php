ea1aa51bb62f64a399897de037cf8d86033bfcf2
ialfaro <iaf643@gmail.com>
2015-12-09 13:51:52 -0600

    added custom panel for cetav_prev-egresados

---
+++ sites/all/themes/cetaviano/layouts/cetav_prev-egresados/cetav-prev-egresados.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Template for a 3 column panel layout.
+ *
+ * This template provides a prev-egresados for CETAV panel display layout.
+ */
+?>
+<div class="panel-display panel-cetav-noticias">
+  <div class="center-wrapper">
+  <div class="grid-width">
+    <div class="panel-container panel-left-column">
+      <?php print $content['left']; ?>
+    </div>
+    <div class="panel-container panel-right-column">
+      <?php print $content['right']; ?>
+    </div>
+  </div>
+  </div>
+</div>
