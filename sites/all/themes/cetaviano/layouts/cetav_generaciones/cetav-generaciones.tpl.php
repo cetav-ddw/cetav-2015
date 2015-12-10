cefbfabab4fa9814247bb2d4d279475bee61a48d
ialfaro <iaf643@gmail.com>
2015-12-09 19:32:35 -0600

    modified layout custom from panls cetav

--- sites/all/themes/cetaviano/layouts/cetav_generaciones/cetav-generaciones.tpl.php
+++ sites/all/themes/cetaviano/layouts/cetav_generaciones/cetav-generaciones.tpl.php
@@ -9,17 +9,12 @@
 <div class="panel-display panel-cetav-generaciones">
   <div class="center-wrapper">
   <div class="grid-width">
-    <div class="panel-container panel-left-column container-news-prev">
+    <div class="panel-container panel-left-column ">
       <?php print $content['left']; ?>
     </div>
-    <div class="panel-container panel-right-column aside">
+    <div class="panel-container panel-right-column">
       <?php print $content['right']; ?>
     </div>
   </div>
   </div>
-  <?php if ($content['wide']): ?>
-    <div class="panel-container panel-wide">
-      <?php print $content['wide']; ?>
-    </div>
-  <?php endif ?>
 </div>
