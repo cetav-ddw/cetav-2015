83b7814edec98054ad61bf6af8429b3415a32659
supervisor <supervisor@b7-14.local>
2015-12-14 17:54:39 -0600

    identación corregida en js

--- sites/all/themes/cetaviano/src-js/script.js
+++ sites/all/themes/cetaviano/src-js/script.js
@@ -1,20 +1,20 @@
 // Manatí Base Theme JS functions
 jQuery(function($) {
-  /*
+    /*
     *** Typing Text Header ***
-  */
-  $("#typed").typed({
-  // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", 
-  // "It <em>types</em> out sentences.", "And then deletes them.",
-  // "Try it out!"],
-    stringsElement: $("#typed-strings"),
-    typeSpeed: 30,
-    backDelay: 500,
-    loop: false,
-    contentType: "html", // or text
-    // defaults to false for infinite loop
-    loopCount: false
-  });
+    */
+    $("#typed").typed({
+        // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", 
+        // "It <em>types</em> out sentences.", "And then deletes them.",
+        // "Try it out!"],
+        stringsElement: $("#typed-strings"),
+        typeSpeed: 30,
+        backDelay: 500,
+        loop: false,
+        contentType: "html", // or text
+        // defaults to false for infinite loop
+        loopCount: false
+    });
 
     $(".reset").click(function(){
         $("#typed").typed("reset");
