2a345fccbf16ac249c210c0bf04db0b4609db15c
ialfaro <iaf643@gmail.com>
2016-01-14 11:02:27 -0600

    fixed js for typed action

--- sites/all/themes/cetaviano/src-js/script.js
+++ sites/all/themes/cetaviano/src-js/script.js
@@ -1,16 +1,100 @@
-// Manatí Base Theme JS functions
-(function($) {
-  //Este js se incorporara en un patron, temporalmente esta asi hasta terminar de incorporar las funciones resantes.
-  $(function() { 
-    $("#show-pay-form").click(function () {
-      $(this).toggleClass("hide-button");
-      $("#course-form").toggleClass("show-course-form");
-      setTimeout(function(){
-        $("#show-pay-form").remove();
-      }, 1000);
-    });
+// Cetaviano Theme JS functions
+(function ($) {
+  jQuery(document).ready(function($) {
+    var cetav = ( function() {
 
-    $("#edit-field-form-nombre-und-0-value").attr("placeholder", "Nombre Completo");
-    $("#edit-field-form-email-und-0-email").attr("placeholder", "Correo Electrónico");
+      function displayFormCourse() {
+        $("#show-pay-form").click(function () {
+          $(this).toggleClass("hide-button");
+          $("#course-form").toggleClass("show-course-form");
+          setTimeout(function(){
+            $("#show-pay-form").remove();
+          }, 1000);
+        });
+
+        $("#edit-field-form-nombre-und-0-value").attr("placeholder", "Nombre Completo");
+        $("#edit-field-form-email-und-0-email").attr("placeholder", "Correo Electrónico");
+      }
+
+      function showMenu() {
+        $("#menu-toggle").click(function() {
+          $(".content > .menu, .views-exposed-form").toggleClass("expandir");
+          $(".main-content").toggleClass("hola");
+        });
+
+      $("#edit-search-api-multi-fulltext").attr("placeholder", "Buscar");
+      }
+
+      function showSubmenu() {
+        $(".nolink").click(function() {
+          $(".content .expandir li.expanded").toggleClass("cero");
+          $(".expanded > ul").toggleClass("show");
+        });
+      }
+
+      function menuToggle() {
+        $(".menu-toggle").click(function() {
+          $(".menu-toggle").toggleClass("on");
+          $(".header").toggleClass("header-style");
+        });
+      }
+
+      function requiredForm() {
+        $("input").attr("required","required");
+        $("#edit-field-form-email-und-0-email").attr("type", "email");
+        $( "#edit-field-form-nombre-und-0-value" ).bind( "keypress", function( event ) {
+          return validateText(event);
+        });
+      }
+
+      function validateText(e) {
+        console.log(e);
+        var tecla = (document.all) ? e.keyCode : e.which;
+        //Tecla de retroceso para borrar, siempre la permite
+        if (tecla===8) {
+          return true;
+        }
+        // Patron de entrada, en este caso solo acepta letras
+        var patron =/[A-Za-z]/;
+        var tecla_final = String.fromCharCode(tecla);
+        return patron.test(tecla_final);
+      }
+
+      function typed() {
+        $("#typed").typed({
+        stringsElement: $("#typed-strings"),
+        typeSpeed: 30,
+        backDelay: 500,
+        loop: false,
+        contentType: "html", // or text
+        loopCount: false
+        });
+
+        $(".reset").click(function(){
+          $("#typed").typed("reset");
+        });
+      }
+
+
+      return {
+        cetavMenuToggle        :  menuToggle,
+        cetavDisplayFormCourse :  displayFormCourse,
+        cetavShowMenu          :  showMenu,
+        cetavShowSubmenu       :  showSubmenu,
+        cetavRequiredForm      :  requiredForm,
+        cetavTyped             :  typed
+      };
+
+    })();
+
+    cetav.cetavMenuToggle();
+    cetav.cetavShowSubmenu();
+    cetav.cetavShowMenu();
+    cetav.cetavDisplayFormCourse();
+    cetav.cetavRequiredForm();
+    cetav.cetavTyped();
+
   });
-})(jQuery);
+
+}(jQuery));
+
