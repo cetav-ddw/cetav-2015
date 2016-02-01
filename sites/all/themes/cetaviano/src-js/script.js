c18f689f23b5ac4a57f7c005e90b849a4e2197fe
Jose Leiva <jdleiva@backcountry.com>
2016-01-31 19:08:35 -0600

    [Theme] JS snippet para smooth scroll

--- sites/all/themes/cetaviano/src-js/script.js
+++ sites/all/themes/cetaviano/src-js/script.js
@@ -107,6 +107,20 @@
     cetav.cetavRequiredForm();
     cetav.cetavTyped();
     cetav.cetavChangePage();
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
   });
 
 }(jQuery));
