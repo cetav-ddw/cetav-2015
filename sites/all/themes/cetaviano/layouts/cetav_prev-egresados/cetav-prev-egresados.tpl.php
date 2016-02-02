5ef428de65d4db3f7d366665614d5f7dcb0975aa
Jose Leiva <jdleiva@backcountry.com>
2016-02-02 17:28:11 -0600

    [Panel] Agrego seccion a Panel Egresados

--- sites/all/themes/cetaviano/layouts/cetav_prev-egresados/cetav-prev-egresados.tpl.php
+++ sites/all/themes/cetaviano/layouts/cetav_prev-egresados/cetav-prev-egresados.tpl.php
@@ -7,14 +7,24 @@
  */
 ?>
 <div class="panel-display panel-cetav-prev-egresados">
-  <div class="center-wrapper">
-  <div class="grid-width">
-    <div class="panel-left-column">
-      <?php print $content['left']; ?>
-    </div>
-    <div class="panel-right-column">
-      <?php print $content['right']; ?>
+  <div class="section-wide section-intro">
+    <div class="grid-width">
+      <div class="panel-left-column">
+        <?php print $content['left_intro']; ?>
+      </div>
+      <div class="panel-right-column">
+        <?php print $content['right_intro']; ?>
+      </div>
     </div>
   </div>
+  <div class="section-wide">
+    <div class="grid-width">
+      <div class="panel-left-column">
+        <?php print $content['left']; ?>
+      </div>
+      <div class="panel-right-column">
+        <?php print $content['right']; ?>
+      </div>
+    </div>
   </div>
 </div>
