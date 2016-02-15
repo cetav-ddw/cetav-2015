0f1483bc12e968ccb8a7d34826a54bdd6183926e
Jose Leiva <jdleiva@backcountry.com>
2016-02-14 21:01:34 -0600

    [Theme] Ajusto estilos listado y nodo noticias

--- sites/all/themes/cetaviano/layouts/cetav_prev-noticias/cetav-prev-noticias.tpl.php
+++ sites/all/themes/cetaviano/layouts/cetav_prev-noticias/cetav-prev-noticias.tpl.php
@@ -8,16 +8,18 @@
 ?>
 <div class="panel-cetav-prev-noticias">
   <?php if ($content['wide']): ?>
-    <div class="panel-container panel-wide panel-header">
+    <div class="panel-header">
       <?php print $content['wide']; ?>
     </div>
   <?php endif ?>
-  <div class="grid-width">
-    <div class="panel-container panel-left-column container-news-prev">
-      <?php print $content['left']; ?>
-    </div>
-    <div class="panel-container panel-right-column aside">
-      <?php print $content['right']; ?>
+  <div class="section-wide">
+    <div class="grid-width">
+      <div class="panel-left-column container-news-prev">
+        <?php print $content['left']; ?>
+      </div>
+      <div class="panel-right-column aside">
+        <?php print $content['right']; ?>
+      </div>
     </div>
   </div>
 </div>
