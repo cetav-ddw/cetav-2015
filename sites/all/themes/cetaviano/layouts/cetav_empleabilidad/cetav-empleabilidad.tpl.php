771fede5b1a0ec866f1c44198f21a99c315adf4b
MariaJoseSolis <majosm96@gmail.com>
2017-11-15 12:20:40 -0600

    Add class to tpl layouts

--- sites/all/themes/cetaviano/layouts/cetav_empleabilidad/cetav-empleabilidad.tpl.php
+++ sites/all/themes/cetaviano/layouts/cetav_empleabilidad/cetav-empleabilidad.tpl.php
@@ -29,7 +29,7 @@
 
   <?php if ($content['wide_first']): ?>
     <div class="section-wide slab-blue">
-      <div class="grid-width">
+      <div class="grid-width grid-quotes-width">
         <?php print $content['wide_first']; ?>
       </div>
     </div>
