2e00a1f3dd31c3c0e2c1a1cb2fdd63f65d137506
MariaJoseSolis <majosm96@gmail.com>
2017-11-15 18:03:20 -0600

    Modified class

--- sites/all/themes/cetaviano/layouts/cetav_egresados/cetav-egresados.tpl.php
+++ sites/all/themes/cetaviano/layouts/cetav_egresados/cetav-egresados.tpl.php
@@ -16,7 +16,7 @@
   <?php endif ?>
   <?php if ($content['wide_first']): ?>
     <div class="section-wide slab-blue">
-      <div class="grid-width grid-quotes-width">
+      <div class="grid-width grid-full-limit">
         <?php print $content['wide_first']; ?>
       </div>
     </div>
