39525d21f6c2205062f7947401f7b1dc6f04e154
Max Mendez <maxmv@maxmendez.net>
2021-06-12 11:23:37 -0600

    Fixed errors
    - Problem with pager and news grid.
    - Problem with link of site logo, that returns to https://www.parquelalibertad.org/

--- sites/all/themes/cetaviano/templates/system/page.tpl.php
+++ sites/all/themes/cetaviano/templates/system/page.tpl.php
@@ -13,7 +13,7 @@
   <div class="section-wide head-nav slab-orange">
     <?php if ($page['navigation']): ?>
     <div class="grid-width">
-        <a href="/" class="header__logo"><img class="icon-cetav" src="<?php echo base_path().path_to_theme() ?>/img/triada-logo.png" alt="Inicio sitio web CETAV"></a>
+        <a href="<?php echo base_path();?>" class="header__logo"><img class="icon-cetav" src="<?php echo base_path().path_to_theme() ?>/img/triada-logo.png" alt="Inicio sitio web CETAV"></a>
         <div class="cont-burger menu-collapse">
           <a id ="menu-toggle" class="menu-toggle">
               <div class="menu-toggle-one"></div>
